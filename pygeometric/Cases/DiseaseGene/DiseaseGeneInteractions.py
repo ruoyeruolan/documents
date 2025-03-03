2746fd4e59fd8f88b362b383a5d08a714a3d484c
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-03-03 21:23:44 +0800

    sync

--- pygeometric/Cases/DiseaseGene/DiseaseGeneInteractions.py
+++ pygeometric/Cases/DiseaseGene/DiseaseGeneInteractions.py
@@ -8,10 +8,13 @@
 @Description: 
 """
 
-from sympy import degree
+from ast import List
 import torch
 import numpy as np
 import pandas as pd
+
+import networkx as nx
+import torch_geometric.transforms as T
 
 from typing import Tuple
 
@@ -20,11 +23,13 @@ from torch_geometric.data import Data
 from torch_geometric.utils import degree
 
 
+
 # _cached_data_: pd.DataFrame | None = None
 
 def load_data(offset: int = 0, index_col: int | str = 0):
-    
-    data= pd.read_csv('./Cases/DiseaseGene/data.tsv', sep='\t', header=0, index_col=index_col)
+
+    path = '/Users/wakala/IdeaProjects/Projects/documents/pygeometric/Cases/DiseaseGene/data.tsv'
+    data= pd.read_csv(path, sep='\t', header=0, index_col=index_col)
     mapping = {index_id: i + offset for i, index_id in enumerate(data.index.unique())}
     return mapping
 
@@ -33,13 +38,14 @@ def load_edge_list(
         dst_col: str, dst_mapping: dict,
 ) -> Tensor:
 
-        data = pd.read_csv('./Cases/DiseaseGene/data.tsv', sep='\t', header=0) 
+        path = '/Users/wakala/IdeaProjects/Projects/documents/pygeometric/Cases/DiseaseGene/data.tsv'
+        data = pd.read_csv(path, sep='\t', header=0) 
         src_nodes = [src_mapping[idx] for idx in data[src_col]]
         dst_nodes = [dst_mapping[idx] for idx in data[dst_col]]
         edge_index = torch.tensor([src_nodes, dst_nodes])
         return edge_index
 
-def initialize_data(num_features: int = 1) -> Tuple[Data, dict, dict]:
+def initialize_data(num_features: int = 20) -> Tuple[Data, dict, dict]:
 
     dz, gene_col = '# Disease ID', 'Gene ID'
     dz_mapping = load_data(index_col=dz)
@@ -54,13 +60,76 @@ def initialize_data(num_features: int = 1) -> Tuple[Data, dict, dict]:
     data = Data()
     data.num_nodes = len(dz_mapping) + len(gene_mapping)
     data.edge_index = torch.cat((edge_index, reverse_edge_index), dim=1)
-    data.x = torch.ones((data.num_nodes, num_features))
+    
+    if data.num_nodes is None:
+        raise ValueError("Number of nodes is not initialized properly")
+    
+    data.x = torch.ones((int(data.num_nodes), num_features))
     return data, dz_mapping, gene_mapping
 
 
-def main():
-     data, dz_mapping, gene_mapping = initialize_data()
-     reverse_dz_mapping = {val: key for key, val in dz_mapping.items()}
-     reverse_gene_mapping = {val: key for key, val in gene_mapping.items()}
+def analysis_graph():
+    df = pd.read_csv('/Users/wakala/IdeaProjects/Projects/documents/pygeometric/Cases/DiseaseGene/data.tsv', sep='\t')
+    data, dz_mapping, gene_mapping = initialize_data()
+    reverse_dz_mapping = {val: key for key, val in dz_mapping.items()}
+    reverse_gene_mapping = {val: key for key, val in gene_mapping.items()}
 
-     degree(data.edge_index[0])
+    if data.edge_index is not None:
+        degrees = degree(data.edge_index[0]).numpy()
+        sorted_degree_i = np.argsort(-1 * degrees)  # sort the tensor in descending and return the index of sorted tensor
+    else:
+        raise ValueError("Edge index is not initialized properly")
+    
+    print('Disease Nodes with highest degree')
+    top_k = 10
+    num_dz_nodes = 0
+
+    for i in sorted_degree_i:
+        if i < 519:
+            node_id = reverse_dz_mapping[i]
+            node_description = df[df['# Disease ID'] == node_id]['Disease Name'].drop_duplicates().values
+            node_degree = degrees[i]
+            print(
+                "node_index=" + str(i), 
+                "node_degree=" + str(int(node_degree)),
+                node_id, 
+                node_description
+            )
+
+            num_dz_nodes += 1
+            if num_dz_nodes >= top_k:
+                break
+    
+    print("\nGene Nodes of highest degree")
+    num_gene_nodes = 0
+    for i in sorted_degree_i:
+        if i > 519:
+            node_id = reverse_gene_mapping[i]
+            node_degree = degrees[i]
+
+            print("node_index=" + str(i), "node_degree=" + str(int(node_degree)), node_id)
+            num_gene_nodes += 1
+            if num_gene_nodes >= top_k:
+                break
+    return None
+
+def data_preparation(data: Data, num_features: int = 20, device: str = 'cpu') -> Tuple[Data, Data, Data]:
+
+    data.x = torch.ones((data.num_nodes if data.num_nodes else 0, num_features))
+
+    transform = T.Compose([
+        T.NormalizeFeatures(),
+        T.ToDevice(device),
+        T.RandomLinkSplit(
+            num_val=.05, 
+            num_test=.15, 
+            split_labels=True,
+            is_undirected=True,
+            add_negative_train_samples=True, 
+        ),
+    ])
+
+    train_data, val_data, test_data = transform(data)
+    return train_data, val_data, test_data
+
+
