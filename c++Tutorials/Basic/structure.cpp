4559ecae37a49f05a030414141a2cd198484ff64
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-03-04 19:06:05 +0800

    sync

---
+++ c++Tutorials/Basic/structure.cpp
@@ -0,0 +1,14 @@
+// -*- encoding: utf-8 -*-
+//
+// @Introduce  : 
+// @File       : structure.cpp
+// @Author     : ryrl
+// @Email      : ryrl970311@gmail.com
+// @Time       : 2025/03/04 07:03
+// @Description: 
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello world";
+}
