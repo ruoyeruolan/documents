cf282f0135253f4c5a01088791a59ca014add52d
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-08-24 20:05:58 +0800

    sync

--- pygeometric/gnn/pubmed.py
+++ pygeometric/gnn/pubmed.py
@@ -10,20 +10,29 @@ import torch
 import torch.nn.functional as F
 
 from torch import Tensor
-from torch_geometric.nn import GCNConv
+from torch_geometric.data import Data
+from torch_geometric.nn import GCNConv,GATConv
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
 from torch_geometric.loader import ClusterData, ClusterLoader
 
+from typing import cast
 
 class GCN(torch.nn.Module):
 
-    def __init__(self, in_channels: int, out_channels: int) -> None:
+    def __init__(self, in_channels: int, out_channels: int, layer: str = 'GCN') -> None:
         super().__init__()
 
+        print(f'Using {layer} layer for GCN model.')
         torch.manual_seed(12345)
-        self.conv1 = GCNConv(in_channels, 16)
-        self.conv2 = GCNConv(16, out_channels)
+        if layer == 'GCN':
+            self.conv1 = GCNConv(in_channels, 16)
+            self.conv2 = GCNConv(16, out_channels)
+        elif layer == 'GAT':
+            self.conv1 = GATConv(in_channels, 16)
+            self.conv2 = GATConv(16, out_channels)
+        else:
+            raise ValueError(f'Layer {layer} not supported.')
     
     def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
 
@@ -37,12 +46,12 @@ def main():
 
     dataset = Planetoid(
         '~/IdeaProjects/Projects/documents/pygeometric/data/planetoid', 
-        name='PubMed', transform=NormalizeFeatures()
+        name='PubMed', transform=NormalizeFeatures(), force_reload=True
     )
     # len(dataset[0])
     # Data(x=[19717, 500], edge_index=[2, 88648], y=[19717], train_mask=[19717], val_mask=[19717], test_mask=[19717])
 
-    data_ = dataset.get(0)
+    data_: Data = cast(Data, dataset[0])
 
     torch.manual_seed(12345)
     data = ClusterData(dataset[0], num_parts=32)
@@ -67,7 +76,7 @@ def main():
     model.eval()
     with torch.no_grad():
         pred = model(data_.x, data_.edge_index).argmax(dim=1)
-        correct = (pred[data_.test_mask] == data_.y[data_.test_mask]).sum()
+        correct = (pred[data_.test_mask] == data_.y[data_.test_mask]).sum()  # type: ignore
         acc = correct.item() / data_.test_mask.sum().item()
         print(f'Test Accuracy: {acc: .4f}')
     
@@ -77,17 +86,17 @@ def main():
 
         optimizer.zero_grad()
         pred = model(data_.x, data_.edge_index)
-        loss = torch.nn.CrossEntropyLoss()(pred[data_.train_mask], data_.y[data_.train_mask])
+        loss = torch.nn.CrossEntropyLoss()(pred[data_.train_mask], data_.y[data_.train_mask])  # type: ignore
         loss.backward()
         optimizer.step()
 
-        correct = pred[data_.train_mask].argmax(dim=1) == data_.y[data_.train_mask]
+        correct = pred[data_.train_mask].argmax(dim=1) == data_.y[data_.train_mask] # type: ignore
         acc = correct.sum().item() / data_.train_mask.sum().item()
         print(f'Epoch: {epoch}, Loss: {loss.item(): .4f}, Accuracy: {acc: .4f}')
 
     model.eval()
     with torch.no_grad():
         pred = model(data_.x, data_.edge_index).argmax(dim=1)
-        correct = (pred[data_.test_mask] == data_.y[data_.test_mask]).sum()
+        correct = (pred[data_.test_mask] == data_.y[data_.test_mask]).sum() # type: ignore
         acc = correct.item() / data_.test_mask.sum().item()
         print(f'Test Accuracy: {acc: .4f}')
